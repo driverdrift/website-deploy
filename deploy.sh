ee58127c523f3def76e7ee8d65ddcc8682da298b
pofohp <46t7hfvm6d@privaterelay.appleid.com>
2026-01-08 22:26:17 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+if [ "$EUID" -ne 0 ]; then
+  echo "请使用 sudo 运行此脚本，或者以 root 用户身份运行。"
+  exit 1
+fi
+
+set -e
+
+apt update
+apt install nginx -y
+apt install mariadb-server -y
+apt install php-fpm php php-mysqli php-json php-curl  -y
