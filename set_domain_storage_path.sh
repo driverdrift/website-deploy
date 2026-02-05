08908ee8596ed222b8da2aefbf091bea9d003e84
driverdrift <kxktb@outlook.com>
2026-02-05 14:38:08 +0800

    Create set_domain_storage_path.sh

---
+++ set_domain_storage_path.sh
@@ -0,0 +1,34 @@
+set_domain_storage_path() {
+	read -p "Please input domain you want to use（press enter to use ip address by default): " DOMAIN
+	DOMAIN=${DOMAIN//[[:space:]]/}
+	if [[ -z "$DOMAIN" ]]; then
+		DIR="/var/www/default"
+		echo "Use default website directory: $DIR"
+	else
+		if _is_valid_domain "$DOMAIN"; then
+			DIR="/var/www/$DOMAIN"
+			echo "Use custom domain directory: $DIR"
+		else
+			exit 1
+		fi
+	fi
+}
+
+_is_valid_domain() {
+	local domain=$1
+	# check domain length
+	if (( ${#domain} > 255 )); then
+		echo "your domain length is too long"
+		return 1
+	fi
+
+	# check domain label
+	if echo "$domain" | grep -Pq '^([a-z0-9-]{0,63}\.)*[a-z0-9]([a-z0-9-]{0,61}[a-z0-9])?\.(?=[a-z0-9-]*[a-z])[a-z0-9][a-z0-9-]{0,62}$'; then
+		echo "Validated Successfully"
+		retutn 0
+	else
+		echo "Invalid domain name"
+		retutn 1
+	fi
+}
+
