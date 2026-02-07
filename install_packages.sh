58b7cdd510a987cc7c6ed7958041c2686bb090fb
driverdrift <kxktb@outlook.com>
2026-02-07 11:42:43 +0800

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -1,19 +1,15 @@
 install_packages() {
 	source ./ensure_bin.sh
 
-	ensure_bin nginx mariadb-server php-fpm php-mysql  # necessary
-	ensure_bin php-curl php-xml php-imagick php-mbstring php-zip php-gd php-intl  # suggest
+	# If PHP is installed first, Apache will be installed as a dependency.
+	# Installing php-fpm first will install PHP without Apache, avoiding conflicts with Nginx.
+	ensure_bin nginx mariadb-server php-fpm php-mysql  # necessary for wordpress
+	ensure_bin php-curl php-xml php-imagick php-mbstring php-zip php-gd php-intl  # suggest for wordpress
+	# https://make.wordpress.org/hosting/handbook/server-environment/#required-extensions
 	# nginx, mariadb-server, php-fpm auto-start and enable on boot by default
 	# sudo systemctl enable service
 	# sudo systemctl start service
-	
-	# php php-mysqli
-	# https://make.wordpress.org/hosting/handbook/server-environment/#required-extensions
-
-	# sudo apt install  php    php-xmlrpc php-soap  \
-#  php-bz2 php-cli php-cgi  -y
-	
-	echo "nginx, mariadb-server, php-fpm installed successfully."
+	echo "nginx, mariadb-server, php-fpm and ohter php extensions installed successfully."
 
 	# Download and extract the latest WordPress into /var/www/$DOMAIN
 	if ! wget -qO- https://wordpress.org/latest.tar.gz | tar -xz -C "$1" --strip-components=1; then
