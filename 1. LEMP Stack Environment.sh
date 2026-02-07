9cf20ac293e56f04e2519ea1f11bde58901a526d
driverdrift <kxktb@outlook.com>
2026-02-07 15:12:21 +0800

    Update 1. LEMP Stack Environment.sh

--- 1. LEMP Stack Environment.sh
+++ 1. LEMP Stack Environment.sh
@@ -1,6 +1,1 @@
-# Check and display the status of common web services
-for service in apache2 nginx php8.2-fpm mariadb; do
-  echo "===== $service status ====="
-  sudo systemctl status $service --no-pager
-  echo
-done
+
