2575a0605b89ad2e227a699f436c03d0011eec37
driverdrift <kxktb@outlook.com>
2026-02-07 13:15:19 +0800

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -23,7 +23,7 @@ install_packages() {
 
 _ensure_apache_absent() {
 	if  command -v apache2 &>/dev/null; then
-		apt purge -y apache2 >/dev/null
+		apt-get purge -y apache2 >/dev/null
 		# Using 'purge' instead of 'remove' ensures that configuration files and related data are deleted.
 		# Specifically, it performs the following actions:
 		# 1. Stops and disables the services:
@@ -44,7 +44,7 @@ _ensure_apache_absent() {
 		# This ensures Apache is fully uninstalled with no residual configuration, logs, or active service entries.
 		# then `systemctl list-unit-files | grep apache` will be empty.
 		
-		apt autoremove -y --purge >/dev/null
+		apt-get autoremove -y --purge >/dev/null
 		# Automatically removes packages that were installed as dependencies but are no longer required.
 		# Specifically, this includes apache2-bin, apache2-data, apache2-utils, and any other automatically installed packages no longer needed.
 		# The '--purge' option ensures that any remaining configuration files for these packages are also deleted.
